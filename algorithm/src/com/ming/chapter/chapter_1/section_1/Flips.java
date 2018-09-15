31b033be45e795b23a0cd3601c68ce83ccf2a179
JMTiantd <291253587@qq.com>
2018-09-15 12:17:52 +0800

    测试

---
+++ algorithm/src/com/ming/chapter/chapter_1/section_1/Flips.java
@@ -0,0 +1,58 @@
+package com.ming.chapter.chapter_1.section_1;
+
+import java.util.Scanner;
+
+import edu.princeton.cs.algs4.Counter;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+/**
+ * 模拟t次抛硬币
+ * @Title: Flips.java 
+ * @Package com.ming.chapter.chapter_1.section_1 
+ * @author 王武明
+ * @date 2018年9月8日 下午8:35:38 
+ * @version V1.0
+ */
+public class Flips
+{
+
+    /** 
+     * @author 王武明
+     * @date 2018年9月8日 下午8:41:34 
+     * @param args
+     * @return void    返回类型 
+     */
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+        Counter counter = new Counter("heads");
+        Counter counter2 = new Counter("tails");
+        for (int i = 0; i < T; i++)
+        {
+            if (StdRandom.bernoulli(0.5))
+            {
+                counter.increment();
+            }else {
+                counter2.increment();
+            }
+        }
+        
+        StdOut.println(counter);
+        StdOut.println(counter2);
+        
+        int d = counter.tally() - counter2.tally();
+        
+        StdOut.println("delta:" + Math.abs(d));
+        
+        Counter c1 = new Counter("one");
+        c1.increment();
+        System.out.println(c1);
+        Counter c2 = c1;
+        c2.increment();
+        System.out.println(c1);
+        
+    }
+    
+}
--- algorithm/src/com/ming/chapter/chapter_1/section_1/Test1.java
+++ algorithm/src/com/ming/chapter/chapter_1/section_1/Test1.java
@@ -7,6 +7,54 @@ public class Test1 {
         int[] a = {1,2,3};
         System.out.println("ss");
         System.out.println(a.length);
+        
+        System.out.println(a[0]);
+        int[] b = a;
+        b[0] = 5;
+        System.out.println(a[0]);
+        
+        change(a);
+        
+        System.out.println(a[0]);
+        
+        Person aPerson = new Person();
+        aPerson.setName("zhangsna");
+        change(aPerson);
+        
+        System.out.println(aPerson.getName());
+        
+        int num = 5;
+        cantChange(num);
+        System.out.println(num);
     }
 
+    public static void cantChange(int a)
+    {
+        a = 456;
+    }
+    
+    public static void change(int[] a)
+    {
+        a[0] = 100;
+    }
+    
+    public static void change(Person p)
+    {
+        p.setName("sss");
+    }
+}
+
+class Person
+{
+    String name;
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    public void setName(String name)
+    {
+        this.name = name;
+    }
 }
